c1a108a5b4b05522f44816aaff0840ea90661634
Peeyush Raghav <peeyushraghava.com>
2024-03-01 11:18:12 +0530

    character dirver

--- pseudo/pcd.c
+++ pseudo/pcd.c
@@ -1,73 +1,126 @@
-#include<linux/module.h>
+#include<linux/module.h>			
 #include<linux/fs.h>
 #include<linux/cdev.h>
-#define Dev_mem_size  512
+#include<linux/uaccess.h>
+#define DEV_MEM_SIZE  512
 
-char device_buffer[Dev_mem_size];
+#undef pr_fmt 
+#define pr_fmt(fmt) "%s:" fmt, __func__	
 
+
+/*pseudo device's memory*/
+char device_buffer[DEV_MEM_SIZE];
+
+/*that holds the device number*/
 dev_t device_number;
 
-struct cdev pcd_cdev;
+/*c_dev varible*/
+struct cdev pcd_cdev;	
 
 loff_t pcd_lseek(struct file *filp, loff_t off, int whence)
 {
-return 0;
+ 	pr_info("lseek requested");
+	return 0;
 }
-ssize_t pcd_read(struct file *filp, char user *buff, size_t count, loff_t *f_pos)
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
 {
-return 0;
-}
-ssize_t pcd_write(struct file *filp, const char user *buff, size_t count, loff_t *f_pos)
-{
-return 0;
+	pr_info("read requested for %zu bytes \n",count);
+	pr_info("current pos: %lld",*f_pos);	
+	
+	/*adjust the count*/
+      	if(*f_pos + count>DEV_MEM_SIZE)
+		count = DEV_MEM_SIZE - *f_pos;
+
+	/*copy to user*/
+	if(copy_to_user(buff,&device_buffer[*f_pos],count)){
+		return -EFAULT;
+	}
+
+	/*update the pos*/
+	*f_pos += count;
+	
+	pr_info("number of bytes successfully read = %zu\n", count);
+	pr_info("updated file pos: %lld", *f_pos);
+	
+	/*return the number of bytes*/
+	return 0;
+	}
+	ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
+	{
+	pr_info("write requested for %zu bytes \n",count);
+        pr_info("current pos: %lld",*f_pos);
+        /*adjust the count*/
+        if(*f_pos + count>DEV_MEM_SIZE)
+                count = DEV_MEM_SIZE - *f_pos;
+	if(!count)
+		return -ENOMEM;
+        /*copy from user*/
+        if(copy_from_user(&device_buffer[*f_pos],buff,count)){
+                return -EFAULT;
+        }
+
+        /*update the pos*/
+        *f_pos += count;
+
+        pr_info("number of bytes successfully written = %zu\n", count);
+        pr_info("updated file pos: %lld", *f_pos);
+	
+	
+	return 0;
 }
 int pcd_open (struct inode *inode, struct file *filp)
 {
-return 0;
+	pr_info("Open is successfull\n");
+	return 0;
 }
 int pcd_release (struct inode *inode, struct file *flip)
 {
-return 0;
+	pr_info("release/close is successfull\n");
+	return 0;
 }
 
-struct file_operations pcd_fops;
+struct file_operations pcd_fops =
 {
-
 	.open = pcd_open,
 	.write = pcd_write,	
 	.read = pcd_read,
-	.lseek = pcd_lseek,
+	.llseek = pcd_lseek,
 	.release = pcd_release,
     	.owner = THIS_MODULE
 };
 
 struct device *device_pcd;
+struct class *class_pcd;
 
 static int __init pcd_driver_init(void)
 {
+	/*1.dynamically allocate the device number*/
         alloc_chrdev_region(&device_number,0,1,"pcd_devices");
-	
-	pr_info("%s: Device_number <MAJOR><MINOR> %d:%d\n",__func__,MAJOR(device_number),MINOR(device_number));
-
-	cdev_init(&pcd_cdev, &pcd_fops);
+	pr_info("Device_number <MAJOR><MINOR> %d:%d\n",MAJOR(device_number),MINOR(device_number));
+        
+	/*2. initialize the cdev structure with fops*/
 	pcd_cdev.owner = THIS_MODULE;
+	cdev_init(&pcd_cdev, &pcd_fops);
 	cdev_add(&pcd_cdev,device_number,1);	
-/*create device class under /sys/class/ */
-	class_pcd = class_create(THIS_MODULE,"pcd_class");
-/*populate the sysfs with device information */
-
-	device_pcd = device_create(class_pcd,NULL,device_number,NULL);	
 	
+	/*create device class under /sys/class/ */
+	class_pcd = class_create("pcd_class");
+	
+	/*populate the sysfs with device information */
+	device_pcd = device_create(class_pcd,NULL,device_number,NULL,"pcd");	
 	pr_info("Module init is successfull\n");
 
 	return 0;
-	
+
 }
 
 static void __exit pcd_driver_cleanup(void)
-
 {
-
+	device_destroy(class_pcd,device_number);
+	class_destroy(class_pcd);
+	cdev_del(&pcd_cdev);
+	unregister_chrdev_region(device_number, 1);
+	pr_info("Module unloaded");
 }
 
 module_init(pcd_driver_init);
